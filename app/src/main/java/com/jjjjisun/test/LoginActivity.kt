ffd920aa4e53411b14e7cc3279eca827b5ffd657
kimjihw <kimal846@naver.com>
2021-10-25 19:32:30 +0900

    JoinLayout

--- app/src/main/java/com/jjjjisun/test/LoginActivity.kt
+++ app/src/main/java/com/jjjjisun/test/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.jjjjisun.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jjjjisun.test.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+	val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_login)
+		setContentView(binding.root)
+
+		binding.join.setOnClickListener {
+			startActivity(Intent(this, JoinActivity::class.java))
+		}
 	}
 }
