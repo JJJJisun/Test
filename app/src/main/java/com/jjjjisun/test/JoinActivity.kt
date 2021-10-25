1572cfe17e9e011b0a66185fb5f8d830e4c3be47
kimjihw <kimal846@naver.com>
2021-10-25 21:03:34 +0900

    JoinLayout

--- app/src/main/java/com/jjjjisun/test/JoinActivity.kt
+++ app/src/main/java/com/jjjjisun/test/JoinActivity.kt
@@ -1,11 +1,60 @@
 package com.jjjjisun.test
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.jjjjisun.test.databinding.ActivityJoinBinding
 
-class JoinActivity : AppCompatActivity() {
+class JoinActivity : AppCompatActivity(), View.OnClickListener {
+
+	val binding by lazy { ActivityJoinBinding.inflate(layoutInflater) }
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_join)
+		setContentView(binding.root)
+
+		runOnUiThread {
+			binding.joinName.setOnClickListener(this)
+			binding.joinId.setOnClickListener(this)
+			binding.joinPw.setOnClickListener(this)
+			binding.joinCheckPw.setOnClickListener(this)
+		}
+
+		binding.signupBtn.setOnClickListener {
+			finish()
+		}
+
+	}
+
+	override fun onClick(v: View?) {
+		when (v?.id) {
+			binding.joinName.id -> {
+				binding.joinName.setBackgroundResource(R.drawable.join_click)
+				binding.joinId.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinPw.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinCheckPw.setBackgroundResource(R.drawable.join_nonclick)
+			}
+			binding.joinId.id -> {
+				binding.joinName.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinId.setBackgroundResource(R.drawable.join_click)
+				binding.joinPw.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinCheckPw.setBackgroundResource(R.drawable.join_nonclick)
+			}
+			binding.joinPw.id -> {
+				binding.joinName.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinId.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinPw.setBackgroundResource(R.drawable.join_click)
+				binding.joinCheckPw.setBackgroundResource(R.drawable.join_nonclick)
+			}
+
+			binding.joinCheckPw.id -> {
+				binding.joinName.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinId.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinPw.setBackgroundResource(R.drawable.join_nonclick)
+				binding.joinCheckPw.setBackgroundResource(R.drawable.join_click)
+			}
+		}
 	}
 }
